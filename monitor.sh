f323cbde3674c54145a8131f221dabda368f2a4a
rzg <rzg_cn@163.com>
2017-03-20 14:57:06 +0800

    add RUN ln -s /lib/ld-linux-armhf.so.3 /lib/ld-linux.so.3

--- monitor.sh
+++ monitor.sh
@@ -1,20 +1,12 @@
-#!/usr/bin/env bash
-./portal
-pid=
-trap '[[ $pid ]] && kill $pid; ./portal -s; exit 0' SIGINT SIGTERM
-while :
-do 
-	sleep 60 & pid=$!
-	wait
-	pid=
+#!/bin/bash
 
-	# 监控重启
-        ps -fe | grep ETMDaemon | grep -v grep
-        if [ $? -ne 0 ]
-        then
-                echo "start process....."
-                ./portal
-        else
-                echo "runing....."
-        fi
+set -e
+
+while true; do
+    pid=`ps -ef|grep vod_httpserver|grep -v grep|awk '{print $2}'`
+    if [ -z "$pid" ]; then
+      ./portal
+    else
+        sleep 60
+    fi
 done
